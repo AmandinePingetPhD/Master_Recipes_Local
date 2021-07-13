3cd69609198cddde1abb0f469cbfeed4fbbce5bd
Amandine Pinget PhD <78495096+AmandinePingetPhD@users.noreply.github.com>
2021-07-13 14:45:29 +0200

    Add JSON class data structure

---
+++ Data_recipes/recipeUS_Data.js
@@ -0,0 +1,9 @@
+class recipeUS {
+    constructor(title, ingredients, instructions, t_recipe, RecipeId) {
+        this.title = title;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+        this.t_recipe = t_recipe;
+        this.RecipeId = RecipeId;
+        }
+}
