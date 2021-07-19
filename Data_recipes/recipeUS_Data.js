58f82e702d1109e4aefd8b8fdf77fb0c735d28b1
Amandine Pinget PhD <78495096+AmandinePingetPhD@users.noreply.github.com>
2021-07-19 12:32:11 +0200

    Add to do - in progress

--- Data_recipes/recipeUS_Data.js
+++ Data_recipes/recipeUS_Data.js
@@ -13,3 +13,5 @@ class recipeUS {
 /// Ajout du nombre d'ingrédients nécessaires pour la recette pour une recherche avancée? ingrédients.length ?
 /// Mise en oeuvre d'une recherche avancée? par nombre d'ingrédients? par plusieurs ingrédients? 
 /// Recette avancée par type de plats? à définir? dans t_recipe...
+/// Faire une bdd avec les proportions d'ingrédients en grammes/ métric au lieu d'impérial
+/// t_recipe à modifier avec le type de recette : Drink / Dish / Desert dans un premier temps puis affiner/détailler
