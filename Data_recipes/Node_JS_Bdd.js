ebbb8477ee42348524d140fc74a913ed58e6abea
Amandine Pinget PhD <78495096+AmandinePingetPhD@users.noreply.github.com>
2021-07-19 12:37:37 +0200

    Modif - in progress

--- Data_recipes/Node_JS_Bdd.js
+++ Data_recipes/Node_JS_Bdd.js
@@ -1,6 +1,18 @@
 //// Node JS file for JSON BDD Recipes
 
+//Import de la BDD
 const fs = require('fs')
 let fichier = fs.readFileSync('recipes_raw_result.json')
 let recipes = JSON.parse(fichier)
+
+//Affichage
 console.log(recipes)
+
+//Modifications / Ajouts 
+let donnees = JSON.stringify(personne)
+
+// fs.appendFile
+
+// fs.writeFileSync('personnage2.json', donnees)
+// readFileSync : pour obtenir le contenu, puis écrire en ajoutant d’abord l’ancien contenu.
+// Ou vous pouvez utiliser la fonction appendFile()
