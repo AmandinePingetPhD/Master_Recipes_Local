f2c15594a4093afd86a168c59684d72167dfee8f
Amandine Pinget PhD <78495096+AmandinePingetPhD@users.noreply.github.com>
2021-08-13 19:32:25 +0200

    Modify app for docker

---
+++ App_Master_Recipes/Master_recipe_search.py
@@ -0,0 +1,177 @@
+#### Master's Recipes - IA de suggestion de recettes v1- avec search #####
+#Python 3.9.6
+#Coding Utf-8
+
+"""
+Fonction de suggestion de recettes
+@author : Amandine Pinget, PhD
+"""
+
+#Initialisation
+from flask import Flask
+app = Flask(__name__)
+
+import os
+fileDir = os.path.dirname(os.path.abspath(__file__))
+
+#Import des packages
+import json
+import random
+from datetime import date
+import re
+
+@app.route("/")
+
+def fonction_master_recipes():
+    """
+    Fonction principale 
+    """
+    #Import des recettes
+    with open('recipes_data.json', 'r') as f:
+        data = json.load(f)
+
+    #Date du jour 
+    today = date.today()
+
+    def choix_recette():
+        """
+        Première version avec choix aléatoire de recette suivant ID/numéro de recette
+        """
+        num = 0
+        # Recettes numérotée de 1 à 39517
+        num = random.randint(1,39517)
+        return num
+
+    def search_recipe(data, kind_dish):
+        """
+        Recherche de recette par mot clé dans le titre uniquement
+        """
+        found_recipe = False
+        
+        key_words = input("What are you looking for? Please enter a recipe key-word: ")
+        
+        # Vérifier si la première lettre est en majuscule : sinon : mettre en majuscule
+        # Vérifier si tout en majuscule : si c'est cas : mettre juste première lettre
+        # Boucle for pour recherche avec des plusieurs mots : "sweet potato"
+
+        key_sep = key_words.split()
+        key_w = ''
+
+        for word in key_sep:
+            if word.islower()==True:
+                key_w += word.capitalize()
+            elif word.isupper()==True:
+                key_w += word.capitalize()
+            else:
+                key_w += word
+        key_w = re.sub(r"(\w)([A-Z])", r"\1 \2", key_w)
+
+        lres = []
+        #Recherche du/des mots-clés dans le titre et impression des recettes résultats
+        for keys in data:
+            if key_w in data[keys]['title'] and kind_dish == data[keys]['t_recipe']:
+                lres.append(data[keys]['RecipeId'])
+                print(data[keys]['RecipeId'], "-", data[keys]['title'], "-", data[keys]['t_recipe'])
+                found_recipe = True
+        print("\n")
+
+        #Choix aléatoire sur les recettes solution? proposer autre choix sinon si premeir choix aléatoire pas satisfaisant?
+        if found_recipe == True:
+            alea = input("Do you want a random choice for your recipe? (Y/N) ")
+            while (alea=='Y' or alea=='y'): #Boucle pour random choice
+                num = random.choice(lres)
+                recipe, dish_type = lecture_info_recette(num, data)
+                print_recette(num, recipe, dish_type)
+                alea = input("Do you want another random choice for your recipe? (Y/N) ") #autres choix aléatoire? Utile ou pas?
+                print("\n")
+            #Choix de la recette + impression / message d'erreur si pas trouvé : nouvelle recherche
+            else:
+                numero = input("What recipe number do you want?") #to improve : click sur num recette?n
+                print("\n")
+                while (numero.isnumeric()!=True or int(numero)>39517):
+                    numero = input("Recipe number is invalid! Could you please enter another recipe number?")
+                num = int(numero)
+                recipe, dish_type = lecture_info_recette(num, data)
+                print_recette(num, recipe, dish_type)
+        elif found_recipe == False:
+            search = input("\n I'm sorry, I haven't found what you're looking for. Another try? (Y/N) \n")
+            if (search=='Y' or search=='y'):
+                search_recipe(data, kind_dish)
+
+    def lecture_info_recette(num, data):
+        """
+        Identification et lecture des infos liées aux recettes
+        """
+        recipe = {}
+        #Recette identifiée avec composants et mise dans le dictionnaire pour affichage
+        for keys in data:
+            if num == data[keys]['RecipeId']:
+                recipe = data[keys]
+                dish_type = recipe['t_recipe']# lecture du type de recette
+                break
+        return recipe, dish_type
+
+    def print_recette(num, recipe, dish_type):
+        """
+        Impression de la recette choisie
+        """    
+        #Impression titre puis ingrédients avec tirets et instructions
+        #Tenir compte du jour pour versions futures au niveau des suggestions
+
+        print("As today is,",today,", I suggest you : ",recipe['title'], "-", dish_type)
+        print("Recipe Number : ", num)
+        print("\n")
+        print("Ingredients :")
+        for el in recipe['ingredients']:
+            print("- ", el)
+        print("\n")
+        print("Instructions :")
+        print(recipe['instructions'])
+        print("\n")
+
+        ##### Export à voir sous un certain format? Modalités? Mail? Historique à garder? 
+        #### Carnet de recettes avec historique? Rating si recette testée? A voir...
+
+    def ask_another_recipe():
+        """
+        Demande si besoin d'une autre recette
+        """
+        #Boucle et suggère d'autres recettes si besoin sinon exit
+        choice = input(" \n Do you want another recipe? (Y/N) ")
+        print("\n")
+        if (choice=='Y' or choice=='y'):
+            fonction_master_recipes()    
+        else:
+            print("Have a nice cooking time and meal! Good bye, see you later ;)", "\n")
+
+
+    ####Exécution du programme
+    
+    #Demande si recherche d'ingrédient/recette particulière
+    search = input("Do you want to search a special recipe? (Y/N) ")
+    print("\n")
+
+    # Demande du type de recette
+    kind_dish = input("What kind of dish go you want to cook? Please provide you choice between: Dish with meat, Dessert, Dessert with fruit, Dish with veggies, Dish with Sea Food/fish, Pasta, Side Dish, Pizza, Drink, Sandwich, Dish with egg, Recipes for dogs ")
+    print("\n")
+    #Liste de choix à cocher?
+
+    #Si oui: fonction de search
+    if (search=='Y' or search=='y'):
+        search_recipe(data, kind_dish)
+    else:   #Si non: recette choisie au hasard
+        num = choix_recette()
+        recipe, dish_type = lecture_info_recette(num, data)
+        #test si bon type de recette  == type demandé
+        while kind_dish != dish_type:
+            num = choix_recette()
+            recipe, dish_type = lecture_info_recette(num, data)
+        else:
+            print_recette(num, recipe, dish_type)
+
+    #Demande si besoin d'une autre recette : si oui: boucle programme si non: exit 
+    ask_another_recipe()
+
+
+if __name__ == "__main__":
+    fonction_master_recipes.run()
